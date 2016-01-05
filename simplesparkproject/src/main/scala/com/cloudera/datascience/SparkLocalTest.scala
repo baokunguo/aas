56291a7fcfa34ac667d6609bb21e6e7e9093b5f3
baobao <shuimuqingyi@163.com>
2016-01-05 23:15:43 +0800

    delete the unnecessary part, such as the META-INF file. make the class MyApp and SparkLocalTest in one jar package

--- simplesparkproject/src/main/scala/com/cloudera/datascience/SparkLocalTest.scala
+++ simplesparkproject/src/main/scala/com/cloudera/datascience/SparkLocalTest.scala
@@ -10,8 +10,7 @@ import scala.math._
 
 object SparkLocalTest {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("Spark Local Pi")
-    .setMaster("local")
+    val conf = new SparkConf().setAppName("Spark Local Pi").setMaster("local")
     val spark = new SparkContext(conf)
     val slices = if(args.length > 0) args(0).toInt else 2
     val n = math.min(100000L * slices, Int.MaxValue).toInt
